94d7fff463145632e8327b6302a8f6687ae56288
Maciej Ziemba <m.ziemba95@gmail.com>
2021-01-23 14:54:31 +0100

    ISSUE-5: Add multiplatform release guard against multiple publications

--- buildata-runtime/build.gradle.kts
+++ buildata-runtime/build.gradle.kts
@@ -44,6 +44,19 @@ kotlin {
     macosX64()
     linuxX64()
     ios()
+    val publicationsFromMainHost =
+            listOf(jvm(), js()).map { it.name } + "kotlinMultiplatform"
+    publishing {
+        publications {
+            matching { it.name in publicationsFromMainHost }.all {
+                val targetPublication = this@all
+                tasks.withType<AbstractPublishToMaven>()
+                        .matching { it.publication == targetPublication }
+                        .configureEach { onlyIf { findProperty("isMainHost") == "true" } }
+            }
+        }
+    }
+
     if (androidEnabled) {
         android {
             publishLibraryVariants("release", "debug")
